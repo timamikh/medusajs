261108143fc83b58097df1c733e8681784fd2fdc
timamikh <tmikhaylov@mail.ru>
2025-08-14 10:59:01 +0300

    Initial Medusa setup with Postgres

--- medusa-config.ts
+++ medusa-config.ts
@@ -12,5 +12,6 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  }
+  },
+  plugins: []
 })
