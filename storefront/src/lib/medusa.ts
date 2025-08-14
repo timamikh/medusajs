658ad51d41b2f29d75ecc307abb96ceaa8b87110
timamikh <tmikhaylov@mail.ru>
2025-08-14 16:25:27 +0300

    Update API key to fix storefront product loading

--- storefront/src/lib/medusa.ts
+++ storefront/src/lib/medusa.ts
@@ -4,7 +4,7 @@ import Medusa from "@medusajs/medusa-js"
 const medusaClient = new Medusa({
   baseUrl: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || "http://localhost:9000",
   maxRetries: 3,
-  publishableApiKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || "pk_test_default",
+  publishableApiKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || "pk_3f97bb73eefcfe9100578b737e13e04f878f0607d080b34d4e01e40facbbab54",
 })
 
 export default medusaClient
